a20b2ce6b156e1e10a3c55813840eb3e221881db
Brandon Manson <mansonbrandon@gmail.com>
2026-08-27 09:44:00 -0400

    test(Pages): reject writable module mode

--- scripts/sitecheck/pages_test.go
+++ scripts/sitecheck/pages_test.go
@@ -38,7 +38,7 @@ func TestSiteCheck_PagesWorkflowRejectsWorkflowAndActionPinMatrix(t *testing.T) 
 		{".github/workflows/pages.yml", "cancel-in-progress: false", "cancel-in-progress: true"},
 		{".github/workflows/pages.yml", "actions/deploy-pages@", "third-party/deploy@"},
 		{".github/workflows/pages.yml", "ruby-version: \"3.3.4\"", "ruby-version: \"3.3\""},
-		{".github/workflows/pages.yml", "GOFLAGS: -mod=mod", "GOFLAGS: -mod=vendor"},
+		{".github/workflows/pages.yml", "GOFLAGS: -mod=readonly", "GOFLAGS: -mod=vendor"},
 		{".github/pages-actions.lock.yml", "45bfe0192ca1faeb007ade9deae92b16b8254a0d", "983d7736d9b0ae728b81ab479565c72886d7745b"},
 		{".github/workflows/pages.yml", "[ \"$mode\" != \"workflow\" ]", "[ \"$mode\" != \"legacy\" ]"},
 		{".github/workflows/pages.yml", "      - uses: actions/configure-pages@", "      - uses: actions/configure-pages@" + "\n        with:\n          static_site_generator: jekyll #"},
